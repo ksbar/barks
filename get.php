b0d30e7951510e1877059f1d6f4993a5a5ca625b
Kansas Bar Association <ksbar@users.noreply.github.com>
2019-02-14 16:58:47 -0600

    Updated to reflect GAE file

--- get.php
+++ get.php
@@ -1,21 +1,25 @@
-<?php
+<?php        
         $http_origin = $_SERVER['HTTP_ORIGIN'];
-        if ($http_origin == "https://www.ksbar.org" || $http_origin == "https://site-ksbar.c9users.io/")
-        {
+        $type = isset($_GET['type']) ? $_GET['type']:'text/plain';
+         if ($http_origin == "https://www.ksbar.org" || $http_origin == "https://site-ksbar.c9users.io" || $http_origin == "https://8080-dot-3118088-dot-devshell.appspot.com" || $http_origin == "https://site-ksbar.appspot.com")
+         {
             header("Access-Control-Allow-Origin: ".$http_origin);
         }
-        header("Content-Type: text/plain");
-
+        header('Access-Control-Allow-Origin: *');
+  		header('Access-Control-Allow-Headers: Content-Length');
+		header('Access-Control-Expose-Headers: Content-Length');
+  		header('Timing-Allow-Origin: *');
+        header("Content-Type: ".$type);
 
         // declare/get a few variables
-        $url = $_GET['url'];
+        $endpoint = isset($_GET['q']) ? $_GET['q']:'https://www.ksbar.org/resource/rss/store.rss';
 
         // create curl resource
         $ch = curl_init();
 
         curl_setopt_array($ch, array(
                 CURLOPT_RETURNTRANSFER => 1,
-                CURLOPT_URL => $url,
+                CURLOPT_URL => $endpoint,
                 CURLOPT_HTTPHEADER => array(
                           //'Accept: application/json' //,
                           //'Authorization: MYTOKEN'
